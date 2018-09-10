1b86bbf421c952a9a489524332681c81943d80ba
lyg <1983654762@qq.com>
2018-09-10 11:33:33 +0800

    m

--- run_server.py
+++ run_server.py
@@ -48,7 +48,8 @@ response = urllib2.urlopen(request)
 print("-" * 30)
 print(response.read())
 print type(response.read())
-code = json.loads(response.read(),encoding='utf-8').get('code')
+res = str(response.read())
+code = json.loads(res,encoding='utf-8').get('code')
 today = str(datetime.now())[:10]
 hao = today[-2:]
 print 'hao:%s'%hao
